65093052f76e66d749e4300bbd32934bb418f029
argahutama <argahut@gmail.com>
2021-02-07 15:06:00 +0700

    Add press back twice to exit app on MainActivity

--- app/src/main/java/com/arga/jetpack/submission2/ui/activity/MainActivity.kt
+++ app/src/main/java/com/arga/jetpack/submission2/ui/activity/MainActivity.kt
@@ -2,10 +2,18 @@ package com.arga.jetpack.submission2.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.content.res.ResourcesCompat
+import com.arga.jetpack.submission2.R
 import com.arga.jetpack.submission2.databinding.ActivityMainBinding
 import com.arga.jetpack.submission2.ui.adapter.SectionPagerAdapter
+import www.sanju.motiontoast.MotionToast
 
 class MainActivity : AppCompatActivity() {
+
+    private var doubleBackToExitPressedOnce = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -17,6 +25,26 @@ class MainActivity : AppCompatActivity() {
             SectionPagerAdapter(this, mFragmentManager)
         activityMainBinding.viewPager.adapter = sectionsPagerAdapter
         activityMainBinding.tabs.setupWithViewPager(activityMainBinding.viewPager)
-        supportActionBar?.elevation = 0f
+    }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+
+        MotionToast.darkColorToast(this,
+                resources.getString(R.string.warning),
+                resources.getString(R.string.press_back_once_again_to_exit),
+                MotionToast.TOAST_WARNING,
+                MotionToast.GRAVITY_BOTTOM,
+                MotionToast.LONG_DURATION,
+                ResourcesCompat.getFont(this,R.font.montserrat))
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            doubleBackToExitPressedOnce = false
+        }, 2000)
     }
 }
