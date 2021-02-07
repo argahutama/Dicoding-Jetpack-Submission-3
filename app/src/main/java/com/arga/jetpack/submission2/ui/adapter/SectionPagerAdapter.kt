224cd75235af8bb5dd4241b2b4f40f12ba102128
argahutama <argahut@gmail.com>
2021-02-07 15:33:24 +0700

    Change color splash & active viewpager

--- app/src/main/java/com/arga/jetpack/submission2/ui/adapter/SectionPagerAdapter.kt
+++ app/src/main/java/com/arga/jetpack/submission2/ui/adapter/SectionPagerAdapter.kt
@@ -1,6 +1,7 @@
 package com.arga.jetpack.submission2.ui.adapter
 
 import android.content.Context
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
@@ -8,25 +9,24 @@ import com.arga.jetpack.submission2.R
 import com.arga.jetpack.submission2.ui.fragment.MovieFragment
 import com.arga.jetpack.submission2.ui.fragment.TvShowFragment
 
-class SectionPagerAdapter(private val mContext: Context, fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-    private val TAB_TITLES = intArrayOf(
-        R.string.tab_movie,
-        R.string.tab_tv_show
-    )
+class SectionPagerAdapter(private val mContext: Context, fm: FragmentManager) :
+        FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(R.string.tab_movie, R.string.tab_tv_show)
+    }
+
     override fun getItem(position: Int): Fragment {
         var fragment: Fragment? = null
         when (position) {
-            0 -> fragment =
-                MovieFragment()
-            1 -> fragment =
-                TvShowFragment()
+            0 -> fragment = MovieFragment()
+            1 -> fragment = TvShowFragment()
         }
         return fragment as Fragment
     }
-    override fun getPageTitle(position: Int): CharSequence {
-        return mContext.resources.getString(TAB_TITLES[position])
-    }
-    override fun getCount(): Int {
-        return 2
-    }
+
+    override fun getPageTitle(position: Int): CharSequence = mContext.resources.getString(TAB_TITLES[position])
+
+    override fun getCount(): Int = TAB_TITLES.size
 }
