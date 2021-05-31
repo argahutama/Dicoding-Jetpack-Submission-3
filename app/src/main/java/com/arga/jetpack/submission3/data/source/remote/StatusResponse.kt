f901c65487e16fb1ed307c5a21c79aa5bb30cc61
Arga Hutama <argahutama>
2021-06-01 00:19:23 +0700

    add api response & status

---
+++ app/src/main/java/com/arga/jetpack/submission3/data/source/remote/StatusResponse.kt
@@ -0,0 +1,7 @@
+package com.arga.jetpack.submission3.data.source.remote
+
+enum class StatusResponse {
+    SUCCESS,
+    EMPTY,
+    ERROR
+}
---
+++ app/src/main/java/com/arga/jetpack/submission3/vo/Status.kt
@@ -0,0 +1,7 @@
+package com.arga.jetpack.submission3.vo
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
