77975fa13d718574fa0efe63554c78101437b422
Arga Hutama <argahutama>
2021-06-01 00:23:33 +0700

    add local repository & app executors in Injection

--- app/src/main/java/com/arga/jetpack/submission3/di/Injection.kt
+++ app/src/main/java/com/arga/jetpack/submission3/di/Injection.kt
@@ -1,11 +1,19 @@
 package com.arga.jetpack.submission3.di
 
+import android.content.Context
 import com.arga.jetpack.submission3.data.DataRepository
-import com.arga.jetpack.submission3.data.source.remote.RemoteRepository
+import com.arga.jetpack.submission3.data.source.local.LocalDataSource
+import com.arga.jetpack.submission3.data.source.local.room.MyDatabase
+import com.arga.jetpack.submission3.data.source.remote.RemoteDataSource
+import com.arga.jetpack.submission3.util.AppExecutor
 
 object Injection {
-    fun dataRepository(): DataRepository {
-        val remoteRepository = RemoteRepository.getInstance()
-        return DataRepository.getInstance(remoteRepository)!!
+    fun provideDataRepository(context: Context): DataRepository {
+        val localRepository = LocalDataSource(
+            MyDatabase.getInstance(context).dao()
+        )
+        val remoteRepository = RemoteDataSource.getInstance()
+        val executors = AppExecutor()
+        return remoteRepository.let { DataRepository.getInstance(localRepository, it, executors)!! }
     }
 }
