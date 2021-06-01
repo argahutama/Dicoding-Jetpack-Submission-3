1036f687fd1453a58eedfa0eaa5fce49e2c11761
Arga Hutama <argahutama>
2021-06-01 15:08:54 +0700

    make entity class readable

--- app/src/main/java/com/arga/jetpack/submission3/data/source/local/entity/MovieEntity.kt
+++ app/src/main/java/com/arga/jetpack/submission3/data/source/local/entity/MovieEntity.kt
@@ -13,24 +13,31 @@ data class MovieEntity(
     @ColumnInfo(name = "id")
     @SerializedName("id")
     val id: Int,
+
     @ColumnInfo(name = "posterpath")
     @SerializedName("poster_path")
     val posterPath: String?,
+
     @ColumnInfo(name = "backdroppath")
     @SerializedName("backdrop_path")
     val backdropPath: String?,
+
     @ColumnInfo(name = "title")
     @SerializedName("title")
     val title: String?,
+
     @ColumnInfo(name = "overview")
     @SerializedName("overview")
     val overview: String?,
+
     @ColumnInfo(name = "releaseddate")
     @SerializedName("release_date")
     val releasedDate: String?,
+
     @ColumnInfo(name = "voteaverage")
     @SerializedName("vote_average")
     val voteAverage: Double?,
+
     @ColumnInfo(name = "isfavorite")
     var isFavorite: Boolean = false
 )
--- app/src/main/java/com/arga/jetpack/submission3/data/source/local/entity/TvShowEntity.kt
+++ app/src/main/java/com/arga/jetpack/submission3/data/source/local/entity/TvShowEntity.kt
@@ -13,24 +13,31 @@ data class TvShowEntity(
     @ColumnInfo(name = "id")
     @SerializedName("id")
     val id: Int,
+
     @ColumnInfo(name = "posterpath")
     @SerializedName("poster_path")
     val posterPath: String?,
+
     @ColumnInfo(name = "backdroppath")
     @SerializedName("backdrop_path")
     val backdropPath: String?,
+
     @ColumnInfo(name = "name")
     @SerializedName("name")
     val name: String?,
+
     @ColumnInfo(name = "firstairdate")
     @SerializedName("first_air_date")
     val firstAirDate: String?,
+
     @ColumnInfo(name = "overview")
     @SerializedName("overview")
     val overview: String?,
+
     @ColumnInfo(name = "voteaverage")
     @SerializedName("vote_average")
     val voteAverage: Double?,
+
     @ColumnInfo(name = "isfavorite")
     var isFavorite: Boolean = false
 )
