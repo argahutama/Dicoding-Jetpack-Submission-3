fc52240942ca382714e2650400f5bd313a43834d
Arga Hutama <argahutama>
2021-06-01 21:18:58 +0700

    make spash activity full screen

--- app/src/main/java/com/arga/jetpack/submission3/presentation/activity/SplashActivity.kt
+++ app/src/main/java/com/arga/jetpack/submission3/presentation/activity/SplashActivity.kt
@@ -4,13 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.arga.jetpack.submission3.R
 import com.arga.jetpack.submission3.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
         val binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
